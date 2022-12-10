946dbfb17fd1a52141a2f6eec649044c217ab880
hannmx <mxkud@mail.ru>
2022-12-11 01:13:20 +0300

    10/10 Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AkkermanFunction(int m, int n)
+{
+    if(m == 0)
+    return n + 1;
+    if(n == 0)
+    return AkkermanFunction(m - 1, 1);
+    if(m > 0 && n > 0)
+    return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    return AkkermanFunction(m, n);
+}
+Console.WriteLine($" A({m},{n}) = {AkkermanFunction(m, n)}");
